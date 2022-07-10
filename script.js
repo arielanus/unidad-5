8a67b00ebd8794bb1d24b24281376f916d61a691
Ariel <arielanus1980@yahoo.com.ar>
2022-07-10 11:42:32 -0300

    ejercicio 5-1

---
+++ script.js
@@ -0,0 +1,7 @@
+var email=prompt("Ingrese email")
+var password=prompt("Ingrese su password")
+
+const correo= "warhol@gmail.com";
+const pass= 12345
+if ( email==correo && password == pass){alert("Ambos datos son correctos")};
+
